aa33acee92c772808a03ff8b22ba37e67a4e625b
Morron17 <tomas.morrone@davinci.edu.ar>
2025-09-08 18:32:39 -0300

    Add files via upload

---
+++ models/InfectedModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const infectedSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: String },
+  role: { type: String },
+  description: { type: String },
+  image: { type: String }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Infected", infectedSchema);
